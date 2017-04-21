2b62e76a7e4eeec7c59d141c20170c38c33c6abf
kuroki <90321panki3z@gmail.com>
2017-04-21 23:03:33 +0900

    change content

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-javascript
+function sikaku1(){
+  document.getElementById('sikaku1').innerHTML = 'こんちは';
+
+}
+
+document.getElementById('sikaku1').addEventListener('mouseover', function(){
+	document.getElementById('sikaku1').innerHTML = '乗った！';
+	document.getElementById('sikaku1').style.background = '#cc9900';
+});
