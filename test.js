336ffe48db3d351f17038fe37bffcaa5f65524cb
kuroki <90321panki3z@gmail.com>
2017-04-25 15:24:29 +0900

    css test
    cssがまともに反応しない

--- test.js
+++ test.js
@@ -1,9 +1,3 @@
 function sikaku1(){
   document.getElementById('sikaku1').innerHTML = 'こんちは';
-
 }
-
-document.getElementById('sikaku1').addEventListener('mouseover', function(){
-	document.getElementById('sikaku1').innerHTML = '乗った！';
-	document.getElementById('sikaku1').style.background = '#cc9900';
-});
